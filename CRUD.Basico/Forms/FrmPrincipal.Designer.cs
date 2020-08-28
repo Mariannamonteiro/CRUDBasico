3dca4c37ec84a8dd6cbc2460bf95e401ad84f16e
Marianna <mamonteiro.oliveira@gmail.com>
2020-08-27 22:35:43 -0300

    Cria formulário de cadastro e classe de acesso a dados.

--- CRUD.Basico/Forms/FrmPrincipal.Designer.cs
+++ CRUD.Basico/Forms/FrmPrincipal.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CkbAtivo = new System.Windows.Forms.CheckBox();
+            this.BtnCadastrar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.BtnCadastrar);
+            this.groupBox1.Controls.Add(this.CkbAtivo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(442, 279);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro de  Aluno";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(420, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(0, 167);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(420, 29);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data de Nascimento:";
+            // 
+            // CkbAtivo
+            // 
+            this.CkbAtivo.AutoSize = true;
+            this.CkbAtivo.Location = new System.Drawing.Point(345, 43);
+            this.CkbAtivo.Name = "CkbAtivo";
+            this.CkbAtivo.Size = new System.Drawing.Size(75, 28);
+            this.CkbAtivo.TabIndex = 4;
+            this.CkbAtivo.Text = "Ativo ";
+            this.CkbAtivo.UseVisualStyleBackColor = true;
+            // 
+            // BtnCadastrar
+            // 
+            this.BtnCadastrar.Location = new System.Drawing.Point(124, 219);
+            this.BtnCadastrar.Name = "BtnCadastrar";
+            this.BtnCadastrar.Size = new System.Drawing.Size(191, 42);
+            this.BtnCadastrar.TabIndex = 5;
+            this.BtnCadastrar.Text = "Cadastrar";
+            this.BtnCadastrar.UseVisualStyleBackColor = true;
+            this.BtnCadastrar.Click += new System.EventHandler(this.BtnCadastrar_Click);
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(466, 303);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmPrincipal";
-            this.Text = "Form1";
+            this.Text = "Sistema N12";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnCadastrar;
+        private System.Windows.Forms.CheckBox CkbAtivo;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CRUD.Basico/Forms/FrmPrincipal.cs
+++ CRUD.Basico/Forms/FrmPrincipal.cs
@@ -16,5 +16,10 @@ namespace CRUD.Basico
         {
             InitializeComponent();
         }
+
+        private void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
